fb1a1078f1ad7da0c4710c2dd019486b1e78b7b7
Birdevinder Singh <Veer davinder>
2019-10-15 17:16:54 -0400

    Array Exercise-2

---
+++ Program4.js
@@ -0,0 +1,8 @@
+//Write a simple JavaScript program to join all elements of the following array into a string.
+
+var joinArr = ["Red","Green","White","Black"];
+
+var listArr = joinArr.join();
+console.log(listArr);
+console.log(listArr.toString());
+console.log(joinArr.join('+'));
